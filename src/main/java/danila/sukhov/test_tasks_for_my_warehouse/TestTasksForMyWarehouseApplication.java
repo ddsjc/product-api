13c3c3886a375f351f8b8c6b06bcce37a55cbf90
ddsjx <cyxob.pycckuu@mail.ru>
2024-11-29 19:47:08 +0400

    Initial commit: A products API implementation project has been created. For a test task

---
+++ src/main/java/danila/sukhov/test_tasks_for_my_warehouse/TestTasksForMyWarehouseApplication.java
@@ -0,0 +1,13 @@
+package danila.sukhov.test_tasks_for_my_warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTasksForMyWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTasksForMyWarehouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/danila/sukhov/test_tasks_for_my_warehouse/TestTasksForMyWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package danila.sukhov.test_tasks_for_my_warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTasksForMyWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
